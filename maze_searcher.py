67afcd0ac990f68a50ae21d34ae166df8743bd76
Jack <jwilliamson4@sheffield.ac.ukgit config --global user.name Jackgit commit -m papersgit config --global user.email jwilliamson4@sheffield.ac.uk>
2018-07-25 02:24:32 +0100

    initial maze generation working

--- maze_searcher.py
+++ maze_searcher.py
@@ -1,12 +1,12 @@
-
+import random
 
 def make_maze():
 
-    maze = Maze([])
+    maze = Maze([], Cell(True, 0, 1))
 
-    for x in range(0, 20):
+    for x in range(0, 10):
         maze.cell_list.append([])
-        for y in range(0, 20):
+        for y in range(0, 10):
             maze.cell_list[x].append(Cell(True, x, y))
 
     print(maze)
@@ -16,34 +16,64 @@ def make_maze():
     possible_to_visit = Stack()
 
     # initial cell.... for now. Know if there are ones which are unvisited by comparing lengths...
-
-    visited_cells.append(maze.cell_list[0][1])
-
+    # marked as visited
     # current cell is the head of the stack
     # x is vertical( rows) , y is across( columns )....
 
-    possible_to_visit.push(maze.cell_list[0][1])
+    # 1.
 
-    while sum(len(x) for x in maze.cell_list) != len(visited_cells):
-        if possible_to_visit.peek().x_cord + 2 < len(maze.cell_list):
+    visited_cells.append(maze.current_cell)
 
-            if maze.cell_list[possible_to_visit.peek().x_cord + 2][possible_to_visit.peek().y_cord] not in visited_cells:
+   # 2.
 
-                possible_to_visit.push(
-                    maze.cell_list[possible_to_visit.peek().x_cord + 2][possible_to_visit.peek().y_cord])
-        if possible_to_visit.peek().x_cord - 2 > 0:
+    for x in range(5):
 
-            if maze.cell_list[possible_to_visit.peek().x_cord - 2][possible_to_visit.peek().y_cord] not in visited_cells:
-                
-                possible_to_visit.push(
-                    maze.cell_list[possible_to_visit.peek().x_cord + 2][possible_to_visit.peek().y_cord])
-        if possible_to_visit.peek().y_cord + 2 < len(maze.cell_list[0]):
-            if maze.cell_list[possible_to_visit.peek().x_cord][possible_to_visit.peek().y_cord + 2] not in visited_cells:
-                print()
-        if possible_to_visit.peek().x_cord - 2 > 0:
-            if maze.cell_list[possible_to_visit.peek().x_cord][possible_to_visit.peek().y_cord - 2] not in visited_cells:
-                print()
+        neighbours_list = []
 
+        if maze.current_cell.x_cord + 2 < len(maze.cell_list):
+
+            if maze.cell_list[maze.current_cell.x_cord + 2][maze.current_cell.y_cord] not in visited_cells:
+
+                neighbours_list.append(maze.cell_list[maze.current_cell.x_cord + 2][maze.current_cell.y_cord])
+
+        if maze.current_cell.x_cord - 2 > 0:
+
+            if maze.cell_list[maze.current_cell.x_cord - 2][maze.current_cell.y_cord] not in visited_cells:
+
+                neighbours_list.append(maze.cell_list[maze.current_cell.x_cord - 2][maze.current_cell.y_cord])
+
+        if maze.current_cell.y_cord + 2 < len(maze.cell_list[0]):
+
+            if maze.cell_list[maze.current_cell.x_cord][maze.current_cell.y_cord + 2] not in visited_cells:
+
+                neighbours_list.append(maze.cell_list[maze.current_cell.x_cord][maze.current_cell.y_cord + 2])
+
+        if maze.current_cell.x_cord - 2 > 0:
+
+            if maze.cell_list[maze.current_cell.x_cord][maze.current_cell.y_cord - 2] not in visited_cells:
+
+                neighbours_list.append(maze.cell_list[maze.current_cell.x_cord][maze.current_cell.y_cord - 2])
+
+        if len(neighbours_list) != 0:
+
+            random_chosen_cell = random.choice(neighbours_list)
+            possible_to_visit.push(maze.current_cell)
+
+            # Remove wall in between by doing the average of them + making the one we go to not a wall
+
+            maze.cell_list[int(maze.current_cell.x_cord+random_chosen_cell.x_cord/2)][int(maze.current_cell.y_cord+random_chosen_cell.y_cord/2)].is_wall = False
+
+            random_chosen_cell.is_wall = False
+
+            maze.current_cell = random_chosen_cell
+            visited_cells.append(maze.current_cell)
+
+        else:
+
+            popped_cell = possible_to_visit.pop()
+            maze.current_cell = popped_cell
+
+    print(maze)
 
 
 class Cell:
@@ -60,8 +90,9 @@ class Cell:
 
 
 class Maze:
-    def __init__(self, cell_list):
+    def __init__(self, cell_list, current_cell):
         self.cell_list = cell_list
+        self.current_cell = current_cell
 
     def __repr__(self):
         return str(self.__dict__)
