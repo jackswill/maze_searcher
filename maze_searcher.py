cf3e2c3da75c1c21cff3e959307139b238463f12
Jack <jwilliamson4@sheffield.ac.ukgit config --global user.name Jackgit commit -m papersgit config --global user.email jwilliamson4@sheffield.ac.uk>
2018-07-26 20:06:27 +0100

    Now finds any vertices (pending path finding)

--- maze_searcher.py
+++ maze_searcher.py
@@ -1,27 +1,29 @@
 import random
 
+
 class MazeSearcher:
 
-    def make_maze():
+    def initialise_filled_maze(height, width, start_point_x, start_point_y):
 
-        maze = Maze([], Cell(True, 1, 1))
+        maze = Maze([], Cell(True, False, start_point_x, start_point_y))
 
-        for x in range(0, 11):
+        for x in range(0, height):
             maze.cell_list.append([])
-            for y in range(0, 51):
-                maze.cell_list[x].append(Cell(True, x, y))
+            for y in range(0, width):
+                maze.cell_list[x].append(Cell(True, False, x, y))
 
         # current cell is the head of the stack
         # x is vertical( rows) , y is across( columns )....
 
         maze.current_cell.is_wall = False
 
-        expanded_maze = MazeSearcher.explore_neighbours(maze)
+        expanded_maze = MazeSearcher.recursive_backtrack(maze)
+        MazeSearcher.find_vertices(expanded_maze)
 
         for i in range(len(expanded_maze.cell_list)):
             print(*expanded_maze.cell_list[i])
 
-    def explore_neighbours(maze):
+    def recursive_backtrack(maze):
 
         visited_cells = [maze.current_cell]
 
@@ -74,18 +76,67 @@ class MazeSearcher:
 
         return maze
 
+    def find_vertices(maze):
+        # Corners are vertices in the Dijkstra algorithm
+        vertices_list = []
+        for row in maze.cell_list:
+            for cell in row:
+                if not cell.is_wall:
+                    vertices = MazeSearcher.corner_information(maze, cell)
+                    if vertices:
+                        vertices_list.append(vertices.corner_type)
+                        cell.is_vertices = True
 
+     #  print(vertices_list)
+
+    def corner_information(maze, cell):
+
+        valid_corner_codes = ["UL", "UR", "DL", "RD", "URD", "RDL", "UDL", "URL", "U", "R", "D", "L"]
+
+        corner_code = ""
+
+        if maze.cell_list[cell.x_cord-1][cell.y_cord].is_wall:
+            corner_code += "U"
+        if maze.cell_list[cell.x_cord][cell.y_cord+1].is_wall:
+            corner_code += "R"
+        if maze.cell_list[cell.x_cord+1][cell.y_cord].is_wall:
+            corner_code += "D"
+        if maze.cell_list[cell.x_cord][cell.y_cord-1].is_wall:
+            corner_code += "L"
+        #print(corner_code)
+
+        if corner_code in valid_corner_codes:
+            return Vertex(False, True, cell.x_cord, cell.y_cord, [], corner_code)
+        else:
+            return False
+
+        
 class Cell:
-    def __init__(self, is_wall, x_cord, y_cord):
+    def __init__(self, is_wall, is_vertices, x_cord, y_cord):
         self.is_wall = is_wall
+        self.is_vertices = is_vertices
         self.x_cord = x_cord
         self.y_cord = y_cord
 
     def __repr__(self):
         if self.is_wall:
             return "#"
+        if self.is_vertices:
+            return "."
         else:
             return " "
+
+
+class Vertex(Cell):
+    # Edges is a set of tuples, representing an edge from a vertex, and its cost to reach it.
+    # corner type allows more efficiency when searching for connecting vertices
+    # corner type list follows following code U = Up cell is wall, L = Left cell is wall etc...
+    # so code URL means Up right and Left occupied (clockwise from Up)
+
+    def __init__(self, is_wall, is_vertices, x_cord, y_cord, edges, corner_type):
+        Cell.__init__(self, is_wall, is_vertices, x_cord, y_cord)
+        self.edges = edges
+        self.corner_type = corner_type
 
 
 class Maze:
@@ -114,4 +165,4 @@ class Stack:
         return len(self.items)
 
 
-MazeSearcher.make_maze()
+MazeSearcher.initialise_filled_maze(11, 51, 1, 1)
